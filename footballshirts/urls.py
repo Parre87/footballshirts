f9c562e637ec19abf343a04b209103c290ef67ab
parre87 <prabath.r@live.se>
2025-08-13 15:41:36 +0200

    Added products view and templates

--- footballshirts/urls.py
+++ footballshirts/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),  # Include home URLs for the home app
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),  # Include product URLs for the products app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files in development
 
